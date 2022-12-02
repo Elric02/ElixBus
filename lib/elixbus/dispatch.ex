50c2dfda5cba7d1d2a15ba7540c4072976593815
Elric02 <elric.milet@gmail.com>
2022-12-02 14:02:48 +0100

    Added basic features for dispatch process

--- lib/elixbus/dispatch.ex
+++ lib/elixbus/dispatch.ex
@@ -1,24 +1,50 @@
 defmodule dispatch do
 
+  # To call for first execution
   def deploy(nb, route) do
     createBus(0, nb, route)
-    manageBus()
+    manageBus(nb)
   end
 
+  # Creates enough bus processes
   def createBus(currentId, nbMax, route) do
     if currentId < nbMax do
-      # fonction bus à importer de bus.ex
-      IO.puts("Creating new bus, id = #{currentId}")
-      Process.register(spawn(__MODULE__, :bus, [route]), String.to_atom("#{currentId}"))¨
+      # (fonction bus à importer de bus.ex)
+      IO.puts("Creating new bus (id #{currentId}) on route #{route}")
+      Process.register(spawn(__MODULE__, :bus, [id, route]), String.to_atom("#{currentId}"))¨
       createBus(currentId + 1, nbMax, route)
     end
   end
 
-  def manageBus() do
+  # Removes specified bus
+  def removeBus(id) do
+    send(String.to_atom("#{id}"), :remove)
+    Process.unregister(String.to_atom("#{id}"))
+  end
+
+  # Receives all messages here
+  def manageBus(nb, route) do
     receive do
-      message ->
-        IO.puts("Message received")
+      {:position, id, pos} ->
+        IO.puts("Position received from #{id} : #{pos}")
+        # TODO : store positions somewhere ? and compute if a bus is late and early
+        manageBus(nb, route)
+      {:change, newNb} ->
+        IO.puts("Change number received. New number : #{newNb}")
+        changeNumber(nb, newNb, route)
     end
+  end
+
+  # Changes the amount of busses on the line.
+  def changeNumber(nb, newNb, route) do
+    if newNb == nb do
+      manageBus(newNb, route)
+    else
+      if newNb > nb do
+        createBus(nb, newNb, route)
+      else
+        removeBus(nb)
+        changeNumber(nb-1, newNb, route)
   end
 
 end
