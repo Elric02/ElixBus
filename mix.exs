e2f6bc0c67cdf52f737c9c379b40ff0923f33099
Elric02 <elric.milet@gmail.com>
2022-12-08 19:02:22 +0100

    Removed json library
    On va utiliser Jason plutôt

--- mix.exs
+++ mix.exs
@@ -48,8 +48,7 @@ defmodule Elixbus.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"},
-	  {:json, "~> 1.4"}
+      {:plug_cowboy, "~> 2.5"}
     ]
   end
 
