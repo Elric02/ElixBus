f91bfb81dfefdac7065375d9f0827ba89cf26df8
BertschL <bertschi.loris@unifr.ch>
2022-12-16 16:19:33 +0100

    Bus should react to stop from dispatch

--- lib/elixbus/bus.ex
+++ lib/elixbus/bus.ex
@@ -1,10 +1,10 @@
 defmodule Bus do
 
   # initialize the route of the bus and its id call bus_deployed
-  # process needs id (?) and a String containing the name of the route in the json file
+  # process needs id (integer) and a String containing the name of the route in the json file
   def bus(id, route) do
     routelistmap = get_route(route)
-    bus_deployed(id, routelistmap, 0, :stop)
+    bus_deployed(id, routelistmap, 0, :stop, 0)
   end
 
   # takes a string, returns a list of maps of the stops
@@ -15,11 +15,20 @@ defmodule Bus do
   end
 
   # should be functionnal except reaction to messages
-  # main function needs id (?), route (list of maps), actual pos (index of the list of maps), and state (atom either :enroute or :stop)
-  def bus_deployed(id, route, pos, state) do
+  # main function needs id (integer), route (list of maps), actual pos (index of the list of maps), and state (atom either :enroute or :stop)
+  def bus_deployed(id, route, pos, state, wait) do
+    send(Dispatch, {:position, id, pos})
+    #record time
+    time_start = Time.utc_now()
 
     {next_pos, next_state} = next(route, pos, state)
-    time_period = goal_time(route, pos, state)
+
+    # calculate the time to wait right now if we're stopped and the next time to wait
+    {time_period,wait} = if state == :stop && wait != 0 do
+      {wait,0}
+    else
+      {goal_time(route, pos, state),wait}
+    end
 
     # waits for orders from the dispatch, else waits time_period ms until next state
     receive do
@@ -27,13 +36,27 @@ defmodule Bus do
         IO.puts("Removed bus #{id}")
         Process.unregister(String.to_atom("#{id}"))
         Process.exit(self(), "ended bus")
+      {:wait, t} ->
+        IO.puts("Dispatch asked to wait for #{t} seconds when it is stopped")
+        {wait_next, wait_now} = if state == :stop do
+          {0,t}
+        else
+          # calculate time left to travel to the next stop
+          to_travel = time_period - Time.diff(time_start,Time.utc_now())
+          {t,to_travel}
+        end
+        Process.sleep(wait_now*1000)
+        # calculate the time it took to change state
+        t = Time.diff(time_start, Time.utc_now())
+        IO.puts("Bus no #{id} was at state #{state} at position #{pos} for #{t} seconds : #{posToString(route,pos)} moving on")
+        bus_deployed(id, route, next_pos, next_state, wait_next)
     after
       time_period ->
-        IO.puts("Bus no #{id} at state #{state} at position #{pos} : #{posToString(route,pos)} moving on")
+        IO.puts("Bus no #{id} was at state #{state} at position #{pos} for #{time_period} seconds : #{posToString(route,pos)} moving on")
         send(:dispatch, {:position, id, pos})
     end
 
-    bus_deployed(id, route, next_pos, next_state)
+    bus_deployed(id, route, next_pos, next_state, wait)
   end
 
   # given a route, a position and a state, it returns the next position and state, for route that are loops only
