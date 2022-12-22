1b94799eea965b7025f92093ffb117f444b727cd
Elric02 <elric.milet@gmail.com>
2022-12-22 02:46:39 +0100

    Corrected bug with display, waiting time calculated

--- lib/elixbus/dispatch.ex
+++ lib/elixbus/dispatch.ex
@@ -4,8 +4,8 @@ defmodule Dispatch do
   def deploy(nb, route) do
     routelistmap = get_route(route)
     createBus(0, nb, route, routelistmap)
-    totallength = calculateTotalLength(routelistmap, 0)
-    IO.inspect(totallength)
+    totallength = computeTime(routelistmap, 0, length(routelistmap))
+    IO.puts("Total length of the route : #{totallength}")
     manageBus(nb, route, routelistmap, totallength, [0, 0, 0, 0, 0])
   end
 
@@ -28,8 +28,27 @@ defmodule Dispatch do
   def manageBus(nb, route, routelistmap, totallength, currentPos) do
     receive do
       {:position, id, pos} ->
-        #IO.puts("Position received from #{id} : #{pos}")
         ElixbusWeb.ElixbusLive.update_table(id, pos)
+        # Which one is the following bus in the order of the list
+        if id == nb - 1 do
+          nextbus = 0
+          # Check if the bus is early compared to the next one on the list
+          timeDiff = computeTime(routelistmap, pos, Enum.at(currentPos, nextbus))
+          if timeDiff < (totallength / nb)-40 do
+            timeToWait = round(((totallength / nb)-40) - timeDiff)
+            IO.puts("Bus no #{id} is too early. Sending command to wait #{timeToWait} seconds")
+            # send(String.to_atom("#{id}"), {:wait, timeToWait})
+          end
+        else
+          nextbus = id + 1
+          # Check if the bus is early compared to the next one on the list
+          timeDiff = computeTime(routelistmap, pos, Enum.at(currentPos, nextbus))
+          if timeDiff < (totallength / nb)-40 do
+            timeToWait = round(((totallength / nb)-40) - timeDiff)
+            IO.puts("Bus no #{id} is too early. Sending command to wait #{timeToWait} seconds")
+            # send(String.to_atom("#{id}"), {:wait, timeToWait})
+          end
+        end
         manageBus(nb, route, routelistmap, totallength, List.replace_at(currentPos, id, pos))
       {:change, newNb} ->
         IO.puts("Change number received. New number : #{newNb}")
@@ -37,7 +56,7 @@ defmodule Dispatch do
     end
   end
 
-  # Changes the amount of busses on the line.
+  # Changes the amount of busses on the line
   def changeNumber(nb, newNb, route, routelistmap, totallength, currentPos) do
     if newNb == nb do
       manageBus(newNb, route, routelistmap, totallength, currentPos)
@@ -52,19 +71,26 @@ defmodule Dispatch do
     end
   end
 
-  # takes a string, returns a list of maps of the stops
+  # Takes a string, returns a list of maps of the stops
   def get_route(route) do
     routes_Str = File.read!("priv/static/assets/routes.json")
     routes_JS = Jason.decode!(routes_Str)
     routes_JS[route]["stops"]
   end
 
-  # returns the total length of the route in seconds
-  def calculateTotalLength(routelistmap, stop) do
-    if stop == length(routelistmap) do
+  # Returns the total length of the route interval in seconds
+  def computeTime(routelistmap, firstStop, lastStop) do
+    # Reached the stopping condition, or else the 2 stops are the same. Also checks the case where lastStop is 0
+    if firstStop == lastStop or (firstStop == length(routelistmap) and lastStop == 0) do
       0
     else
-      Enum.at(routelistmap, stop)["trip"] + Enum.at(routelistmap, stop)["stop"] + calculateTotalLength(routelistmap, stop+1)
+      # Reached the end of the route, loop back to the first stop
+      if firstStop == length(routelistmap) do
+        Enum.at(routelistmap, 0)["trip"] + Enum.at(routelistmap, 0)["stop"] + computeTime(routelistmap, 1, lastStop)
+      # Nothing particular, proceed to next stop calculation
+      else
+        Enum.at(routelistmap, firstStop)["trip"] + Enum.at(routelistmap, firstStop)["stop"] + computeTime(routelistmap, firstStop+1, lastStop)
+      end
     end
   end
 
